9adc4cd28f75515b2990379a7e4814fffa97b0f8
IvanMM27 <112850777+IvanMM27@users.noreply.github.com>
2024-10-04 11:43:43 +0200

    new trigger extractor with DOM, PMT, DU ids included. A dictionary with {DOM: DU} ids can be provided

--- src/graphnet/data/extractors/km3net/km3netroottriggpulseextractor.py
+++ src/graphnet/data/extractors/km3net/km3netroottriggpulseextractor.py
@@ -1,6 +1,6 @@
 """Code for extracting the triggered pulse information from a file."""
 
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 import numpy as np
 import pandas as pd
 
@@ -10,15 +10,17 @@ from graphnet.data.extractors.km3net.utilities.km3net_utilities import (
     create_unique_id,
     assert_no_uint_values,
     creating_time_zero,
+    remove_duplicated_event_no,
 )
 
 
-class KM3NeTROOTTriggPulseExtractor(KM3NeTROOTExtractor):
+class KM3NeTROOTTriggPulseExtractor_detector(KM3NeTROOTExtractor):
     """Class for extracting the triggered pulse information from a file."""
 
-    def __init__(self, name: str = "trigg_pulse_map"):
-        """Initialize the class to extract the triggered pulse information."""
+    def __init__(self, name: str = "pulse_map", DOMs_dict: Optional[dict] = None):
+        """Initialize the class to extract the pulse information."""
         super().__init__(name)
+        self.DOMs_dict = DOMs_dict
 
     def __call__(self, file: Any) -> Dict[str, Any]:
         """Extract pulse map information and return a dataframe.
@@ -34,7 +36,7 @@ class KM3NeTROOTTriggPulseExtractor(KM3NeTROOTExtractor):
 
         return pulsemap_df
 
-    def _extract_pulse_map(_: Any, file: Any) -> pd.DataFrame:
+    def _extract_pulse_map(self: Any, file: Any) -> pd.DataFrame:
         """Extract the pulse information and assigns unique IDs.
 
         Args:
@@ -45,25 +47,31 @@ class KM3NeTROOTTriggPulseExtractor(KM3NeTROOTExtractor):
 
         Analogous to the KM3NeTROOTPulseExtractor but doing cuts on trig.
         """
-        primaries = file.mc_trks[:, 0]
+        
         unique_id = create_unique_id(
-            np.array(primaries.pdgid),
             np.array(file.run_id),
             np.array(file.frame_index),
             np.array(file.trigger_counter),
         )  # extract the unique_id
 
+        unique_id_df = pd.DataFrame()
+        unique_id_df['event_no'] = unique_id
+        unique_id_df = remove_duplicated_event_no(unique_id_df, col = 'event_no', keep = 'first')
+        unique_id = unique_id_df.values
+        
         hits = file.hits
         keys_to_extract = [
-            "t",
-            "pos_x",
-            "pos_y",
-            "pos_z",
-            "dir_x",
-            "dir_y",
-            "dir_z",
-            "tot",
-            "trig",
+                                "t",
+                                "pos_x",
+                                "pos_y",
+                                "pos_z",
+                                "dir_x",
+                                "dir_y",
+                                "dir_z",
+                                "tot",
+                                "trig",
+                                "dom_id",
+                                "channel_id"
         ]
 
         pandas_df = hits.arrays(keys_to_extract, library="pd")
@@ -75,6 +83,10 @@ class KM3NeTROOTTriggPulseExtractor(KM3NeTROOTExtractor):
 
         # keep only trigg pulses
         df = df[df["trig"] != 0]
+        
+        # Add the du_id
+        if self.DOMs_dict) is not None:
+            df["du_id"] = list(map(self.DOMs_dict.get, df["dom_id"].astype(str)))
 
         df = df.drop(["entry", "subentry"], axis=1)
         df = creating_time_zero(df)
