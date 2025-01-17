44b141ed67247dd5b08dd96f4f52f19edf799dc5
Jorge Prado Gonzalez <jprado@glui01.ific.uv.es>
2025-01-17 12:58:25 +0100

    km3net extraction example added

---
+++ examples/07_km3net/01_convert_km3net.py
@@ -0,0 +1,88 @@
+import os
+import warnings
+
+from graphnet.constants import EXAMPLE_OUTPUT_DIR, TEST_DATA_DIR
+from graphnet.data.readers import KM3NeTReader
+from graphnet.data.writers import ParquetWriter, SQLiteWriter
+from graphnet.data import DataConverter
+from graphnet.data.extractors.km3net import (
+    KM3NeTTruthExtractor,
+    KM3NeTFullPulseExtractor,
+    KM3NeTTriggPulseExtractor,
+    KM3NeTHNLTruthExtractor,
+    KM3NeTRegularRecoExtractor,
+    KM3NeTHNLRecoExtractor,
+)
+
+from graphnet.utilities.argparse import ArgumentParser
+import sys
+
+
+def main(
+    backend: str,
+    triggered: str,
+    HNL: str
+) -> None:
+    """Convert ROOT files from KM3NeT to `backend` format."""
+
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    
+    #input_dir = [f"{TEST_DATA_DIR}/km3net"]
+    input_dir = "/lustre/ific.uv.es/prj/gl/vega/jprado/hnl_files/neutrino_fetch/iRods/review_debug"
+    outdir = f"{EXAMPLE_OUTPUT_DIR}/{backend}"
+    os.makedirs(outdir, exist_ok=True)
+
+    if backend == "parquet":
+        save_method = ParquetWriter(truth_table="truth")
+    elif backend == "sqlite":
+        save_method = SQLiteWriter()
+
+    if HNL == "km3net-vars":
+        truth_extractor = KM3NeTTruthExtractor(name = "truth")
+        reco_extractor = KM3NeTRegularRecoExtractor(name = "reco")
+    elif HNL == "hnl-vars":
+        truth_extractor = KM3NeTHNLTruthExtractor(name = "truth")
+        reco_extractor = KM3NeTHNLRecoExtractor(name = "reco")
+
+    if triggered == "Triggered":
+        pulse_extractor = KM3NeTTriggPulseExtractor(name = "pulse_map")
+    elif triggered == "Snapshot":
+        pulse_extractor =  KM3NeTFullPulseExtractor(name = "pulse_map")
+
+    
+
+    converter = DataConverter(
+        file_reader = KM3NeTReader(),  
+        save_method = save_method,
+        extractors = [
+                     truth_extractor,
+                     pulse_extractor,
+                     reco_extractor
+        ],
+        outdir = outdir,
+        num_workers = 1
+    )
+
+    converter(input_dir=input_dir)
+
+if __name__ == "__main__":
+
+    # Parse command-line arguments
+    parser = ArgumentParser(
+        description="""
+            Convert root files from KM3NeT to an sqlite or parquet.
+            """
+    )
+
+    parser.add_argument("backend", choices=["sqlite", "parquet"], help="Choose the backend format")
+    parser.add_argument("triggered", choices=["Triggered", "Snapshot"], help="Choose between triggered or snapshot pulse maps")
+    parser.add_argument("HNL", choices=["km3net-vars", "hnl-vars"], help="Km3net truth or adding Heavy Neutral Lepton info")
+
+    args, unknown = parser.parse_known_args()
+
+    # Run example script
+    main(
+        args.backend,
+        args.triggered,
+        args.HNL
+        )
---
+++ examples/07_km3net/README.md
@@ -0,0 +1,25 @@
+# KM3Net Data Conversion
+
+This file is designed to extract information from ROOT files of KM3Net into intermediate file formats suitable for deep learning.
+
+## Example Usage
+
+To convert KM3Net data, you can use the following command:
+
+```bash
+python 01_convert_km3net.py sqlite Triggered km3net-vars
+```
+
+## Help
+
+For more information on the available options for the parser, you can use the help flag:
+
+```bash
+python 01_convert_km3net.py -h
+```
+
+or
+
+```bash
+python 01_convert_km3net.py --help
+```
